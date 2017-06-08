fb435ff0e877671bb3277dfa5dffa139bed42cbc
Johanna Bohn <johanna.e.bohn@gmail.com>
2017-06-08 15:53:42 +0200

    fixed assignment 3, part c, now finished

--- Kleemann_Bambauer_Bohn_Blatt6.R
+++ Kleemann_Bambauer_Bohn_Blatt6.R
@@ -1,7 +1,7 @@
 
 # a)
 quantil_exp <- function(t,l) {
-    return ((-1/l)*log(1-t)) # taken from Bsp 1.32
+    return ((-1/l)*log(1-t)) # taken from example 1.32
 }
 
 # b)
@@ -10,9 +10,28 @@ empVF <- function(x,t) {
     return (length(y)/length(x))
 }
 
-# c) broken as shit
+# c)
+# create 1000 uniformly distributed ts
 t = runif(1000, min=0, max=1)
+# use ts to compute 1000 exponentially distributed qs
 q = quantil_exp(t, (1/2))
-f = empVF(q, t)
-x = c(0:10)
-plot(x, f, col="red",exp(-(1/2)*x), col="blue")
+f = c()
+# amounnt of values to be computed
+n = 100
+# create vector of n vaöues between 0 and 10
+x = seq(0,10,length.out = n)
+# for each of those x-values, compute the corresponding f(x)-value
+for (i in 0:n) {
+    f[i] = empVF(q, x[i])
+}
+# plot computed distribution in red, theoretical distribution in blue
+plot(x, f, col="red", type = "l", 
+     main = "Exponentialfunktion", 
+     xlab = "x-Werte",
+     ylab = "y-Werte")
+# add line of theoretical distribution to plot
+lines(x, exp(-(1/2)*x), col="blue", type = "l")
+# add legend to plot
+legend("right",
+       c("Theoretische Verteilungsfunktion", "Empirische Verteilungsfunktion"),
+       lty=c(1,1), lwd=c(2.5,2.5), col=c("blue","red"))
